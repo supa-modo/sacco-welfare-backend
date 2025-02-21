0ba2e9b7ba0ded651eaf7cf95c7ddb1eae9c3fbc
supa-modo <eddieochieng11@gmail.com>
2025-02-21 13:49:16 +0300

    Removed temporary database sync force from server backend

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ const startServer = async () => {
       console.log('Database synced successfully');
     }
 
-    await sequelize.sync({ alter: true });
-      console.log('Database synced successfully');
+    // await sequelize.sync({ alter: true });
+    //   console.log('Database synced successfully');
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
